b1fbca8a17dd0f8661b1679d09ed8105c27d5b67
Vinicius Levi <levi.vinicius@yahoo.com.br>
2021-06-11 00:43:06 -0300

    salvando os valores de cadastrarServico

--- codigo/JavaScript/cadastroServico.js
+++ codigo/JavaScript/cadastroServico.js
@@ -34,14 +34,34 @@ function salvaDados() {
     //const getDia = document.querySelector('#Dia');
     const getData = document.querySelector('#data');
     const getTempo = document.querySelector('#tempo');
+    
     if (getAtuacao != null) {
         var atuacao = getAtuacao.value;
-        console.log(atuacao)
+        console.log(`atuacao ${atuacao}`)
+    }
+    if (document.querySelector('#Dia') != null) {
+        const getDia = document.querySelector('#Dia');
+        const dia = getDia.value;
+        console.log(`Dia ${dia}`)
+    }
+    if (document.querySelector('.admin') != null) {
+        const getAdmin = document.querySelector('.admin');
+        const admin = getAdmin.value;
+        console.log(`admin ${admin}`)
+    }
+    if (document.querySelector('.educacao') != null) {
+        const getEducacao = document.querySelector('.educacao');
+        const educacao = getEducacao.value;
+        console.log(`educacao ${educacao}`)
+    }
+    if (document.querySelector('.casa') != null) {
+        const getCasa = document.querySelector('.casa');
+        const casa = getCasa.value;
+        console.log(`casa ${casa}`)
     }
     if (document.querySelector('#taxa') != null) {
         const getTaxa = document.querySelector('#taxa');
         const taxa = Number(getTaxa.value);
-        console.log(taxa);
         if (!taxa || taxa < 0) {
             if (taxa != null) {
                 alert('A taxa deve ter valor numérico');
@@ -53,6 +73,19 @@ function salvaDados() {
         }
         console.log(taxa);
     }
+    
+    
+}
+
+function verificaData(){
+    if (document.querySelector('#data') != null) {
+        alert('Favor selecionar a data')
+    }
+}
+
+
+
+function verificaRegiao() {
     if (document.querySelector("#regiao") != null) {
         const getRegiao = document.querySelector("#regiao");
         const regiao = String(getRegiao.value);
@@ -61,18 +94,20 @@ function salvaDados() {
             return;
         }
         if (regiao == null) {
+            
+            input.addEventListener('Submit', function(e) {
+            const input = document.querySelector('.btn');
+            e.preventDefault();
             alert('Favor preencher a região')
+            })
+            alert('Favor preencher a região')
+            console.log(regiao);
+            input.preventDefault();
             return;
         }
-        console.log(regiao);
-    }
-
-
-    if (document.querySelector('#Dia') != null) {
-        const getDia = document.querySelector('#Dia');
-        const dia = getDia.value;
-        console.log(dia);
+    console.log(regiao);
     }
 }
+
 
 salvaDados();
