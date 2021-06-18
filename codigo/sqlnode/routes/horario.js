069377b7c83ecd528f9bd7368f1b2c44fb688d1b
andre1202002 <andre.cota@hotmail.com>
2021-06-18 00:02:19 -0300

    Alteracoes base back end

--- codigo/sqlnode/routes/horario.js
+++ codigo/sqlnode/routes/horario.js
@@ -3,31 +3,21 @@ const express = require('express');
 const router = express.Router();
 
 //const controllers = require('../controllers');
-const ordemProducaoController = require('../controllers/ordemProducao');
-//get,post(criar),put(atualizar),delete
+const ordemController = require('../controllers/ordemProducao');
+
 router.get('/', (req, res) => {
-  controllers.execute(req, res, ordemProducaoController.getOrdemProducao);
+  controllers.execute(req, res, ordemController.getOrdemProducao);
 });
+// get,post(criar),put(atualizar),delete
 // index buscar todos
 // store salvar dado novo
 // update alterar dados
-// delete
-router.get('/', ordemProducaoController.index);
+// delete deletar
+router.get('/', ordemController.list);
 
 
-router.put('/marcaOrdemComoImpostada', (req, res) => {
-  req.body.status = 'EM_PRODUCAO';
-  controllers.execute(req, res, ordemProducaoController.atualizaOrdemProducao);
-});
-
-router.put('/marcaOrdemComoDeliberada', (req, res) => {
-  req.body.status = 'CONCLUIDA';
-  controllers.execute(req, res, ordemProducaoController.atualizaOrdemProducao);
-});
-
-router.put('/marcaOrdemComoAguardando', (req, res) => {
-  req.body.status = 'AGUARDANDO';
-  controllers.execute(req, res, ordemProducaoController.atualizaOrdemProducao);
+router.store('/insereCadastro', (req, res) => {
+  controllers.execute(req, res, ordemController.atualizaOrdemProducao);
 });
 
 module.exports = router;
